618edf1fc2b2a9be403f77806ebbb88ea1bf0b7c
GT007-tech <80970496+GT007-tech@users.noreply.github.com>
2021-03-20 21:48:02 +0530

    finding out perfect squares from 0 to given number

--- 1.Perfect square from 1 to n.py
+++ 1.Perfect square from 1 to n.py
@@ -1,4 +1,4 @@
-from math import sqrt,floor
+from math import sqrt,floor # to import specific methods from a module
 n = int(input('please enter the number upto which you want to find the perfect squares:'))
 print("the following are perfect squares from 1 to", n)
 for i in range(1,n+1,1):
