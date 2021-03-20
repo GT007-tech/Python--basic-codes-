9f0b1794886412405d7644d66668c2c789a6b681
GT007-tech <80970496+GT007-tech@users.noreply.github.com>
2021-03-20 21:46:30 +0530

    Add files via upload

---
+++ 6.Perfect square from 1 to n.py
@@ -0,0 +1,6 @@
+from math import sqrt,floor
+n = int(input('please enter the number upto which you want to find the perfect squares:'))
+print("the following are perfect squares from 1 to", n)
+for i in range(1,n+1,1):
+    if sqrt(i) == floor(sqrt(i)):
+        print(i,",",end='' )
